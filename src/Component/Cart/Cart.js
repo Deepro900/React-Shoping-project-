9db7977f15e795abfe7ce7be6c517fee66a7df9e
Deepro900 <deeprobhattacharjee@gmail.com>
2022-10-08 13:49:44 +0600

    added the cart

---
+++ src/Component/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            <h3>Order Summary </h3>
+            <p>select items:</p>
+        </div>
+    );
+};
+
+export default Cart;
